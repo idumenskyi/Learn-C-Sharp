0dcbd3ff667fc98df3f4b1643a7a30d590e9d1a1
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-11-14 13:12:02 +0200

    Create 006_for_loop.cs

---
+++ 006_for_loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LearnCSharp
+{
+    public class ForLoop
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
