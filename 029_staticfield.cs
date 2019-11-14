6c611b08ef0ec82462005e210700f445a93f6793
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-11-14 22:47:49 +0200

    Create 029_staticfield.cs

---
+++ 029_staticfield.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace LearnCSharp
+{
+    public class Account
+    {
+        public int accno;
+        public String name;
+        public static float rateOfInterest = 8.8f;
+        public Account(int accno, String name)
+        {
+            this.accno = accno;
+            this.name = name;
+            
+        }
+        public void display()
+        {
+            Console.WriteLine(accno + " " + name + " " + rateOfInterest);
+        }
+    }
+    class SomeEmployee
+    {
+        public static void Main(string[] args)
+        {
+            Account a1 = new Account(100, "Account1");
+            Account a2 = new Account(101, "Account2");
+            a1.display();
+            a2.display();
+
+        }
+    }
+}
