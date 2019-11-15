bb46c448c4682cdcf41bc9d3815629190d9da7ae
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-11-15 03:31:40 +0200

    Create 030_staticclass.cs

---
+++ 030_staticclass.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace LearnCSharp
+{
+
+
+    public class MyMath
+    {
+        public static float PI = 3.14f;
+        public static int cube(int n) 
+        { 
+            return n * n * n; 
+        }
+        
+    }
+    class TestMyMath
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Value of PI is: " + MyMath.PI);
+            Console.WriteLine("Cube of 3 is: " + MyMath.cube(3));
+
+        }
+    }
+}
